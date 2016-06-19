ec8453f564ed28a7263fa371361fb27c24864766
backpackcoder <andrew.hammer@backpackcoder.com>
2016-06-18 21:48:41 -0400

    Added header

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,12 @@ gulp.task('build-dev', shell.task(
 
 gulp.task('watch', shell.task([
     'docker rm -f dev',
-    'open http://192.168.99.100:3000/map.html',
-    'docker run -i --rm'
+    'docker run -d'
         + ' --name dev'
         + ' -v $PWD/app:/usr/src/app'
         + ' -w /usr/src/app'
         + ' -p 3000:3000'
-        + ' wif/dev'],
-    { interactive: true, verbose: true, ignoreErrors: true }
+        + ' wif/dev',
+    'open http://192.168.99.100:3000/map.html'],
+    { verbose: true, ignoreErrors: true }
 ));
