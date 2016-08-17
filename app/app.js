6a04fc7d5c54123a82ae7405d0545be0f2f39ff5
backpackcoder <andrew@the-hammers.net>
2016-08-17 23:26:31 +0300

    Updated counter code

--- app/app.js
+++ app/app.js
@@ -3,10 +3,12 @@ var staticZip = require('express-static-zip');
 
 var app = express();
 app.use(express.static('.'));
+
+// Attach tile endpoints
 app.use('/tiles', staticZip('./tiles/0-8.zip'));
 app.use('/tiles', staticZip('./tiles/9.zip'));
-app.use('/', staticZip('./counters/counters.zip'));
 
+// Attach counter endpoints
 var AdmZip = require('adm-zip');
 
 // reading archives 
@@ -14,21 +16,35 @@ var zip = new AdmZip('./counters/counters.zip');
 var zipEntries = zip.getEntries(); // an array of ZipEntry records 
 var counters = [];
 zipEntries.forEach(function(zipEntry) {
-    if (!zipEntry.isDirectory) {
+    if (!zipEntry.isDirectory &&
+        (zipEntry.name.indexOf('B.gif') === -1) &&
+        (zipEntry.name.indexOf('Action') !== 0) &&
+        (zipEntry.name.indexOf('PatiF') === -1) &&
+        (zipEntry.name.indexOf('LiF') === -1) &&
+        (zipEntry.name.indexOf('DoD') === -1) &&
+        (zipEntry.name.indexOf('PoiF') === -1) &&
+        (zipEntry.name.indexOf('WTHER') === -1) &&
+        (zipEntry.name.indexOf('FiF') === -1) &&
+        (zipEntry.name.indexOf('MiF') === -1) &&
+        //(zipEntry.name.indexOf('USSR') === 0) &&
+        (zipEntry.name.indexOf('Offensive') === -1)
+    ){
         counters.push({
             'name' : zipEntry.name.replace('.gif',''),
-            'url'  : zipEntry.entryName,
+            'url'  : zipEntry.entryName.replace('.gif', 'B.gif'),
         });
     }
 });
+console.log('Found ' + counters.length + ' counters');
 
 // Allow us to get the name of the counters
-var counterEnpoint = express.Router();
-counterEnpoint.get('/', function(req,res){
-    return res.json(counters);
-});
+app.use('/', staticZip('./counters/counters.zip'));
+app.use('/counters', express.Router().get('/', function(req, res){
+    console.log('counters request query string:', req.query);
+    console.log('counters request params:', req.params);
 
-app.use('/counters', counterEnpoint);
+    return res.json(counters);
+}));
 
 
 app.listen(3000, function () {
